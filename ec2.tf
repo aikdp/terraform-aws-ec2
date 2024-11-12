40a2b826392fea70a91be0a22122eea44eaa6a21
aikdp <kdprasad028@gmail.com>
2024-11-12 17:22:53 +0530

    terraform module testing practice

--- ec2.tf
+++ ec2.tf
@@ -1,34 +1,10 @@
 
 resource "aws_instance" "terraform" {
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-
+    ami = var.ami_id
+    instance_type = var.insta_type
+    vpc_security_group_ids = var.sg_group_id
     tags = {
         Name = "Terraform_server"
     }
 } 
-resource "aws_security_group" "allow_ssh_terraform" {
-    name = "allow-port-22"
-    description = "Allow port 22 for ssh access"
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-
-    tags = {
-        Name = "Terraform_SG_SSH"
-    }
-}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "insta_type" {
+    # default = "t3.micro"
+        validation {
+            condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.insta_type)
+            error_message = "The insta_type must be one of these: 't3.micro', 't3.small', 't3.medium'"
+  }
+}
+
+variable "ami_id" {
+    # default = "ami-09c813fb71547fc4f"
+}
+
+variable "sg_group_id" {
+
+    type = list(string)
+}
+
