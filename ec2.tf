963b6d9356d02228e728a85c652f14ff5d1c861f
aikdp <kdprasad028@gmail.com>
2025-03-03 11:20:10 +0530

    ec2 tf volume 50gb practice

--- ec2.tf
+++ ec2.tf
@@ -6,5 +6,11 @@ resource "aws_instance" "terraform" {
     tags = {
         Name = "Terraform_server"
     }
+
+    root_block_device {
+        volume_type = "gp3"
+        volume_size = 50
+        delete_on_termination  = true
+    }
 } 
 
